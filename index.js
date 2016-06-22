75031da9eb810b3dfef5395ee049092914d5aebc
Rafael 'fumasa' Giorgetti <fumasa@wakedown.org>
2016-06-21 21:22:39 -0300

    fix standardjs

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-var request = require("request")
+var request = require('request')
 
 module.exports = {
-  get: function (url, fn, error_fn) {
+  get: function (url, fn, errorFn) {
     request({
-        url: url,
-        method: 'GET',
-        json: true
-      }, function (error, response, body){
-        if (error) {
-          console.error({ ajaxcall: 'get' }, error)
-          if (error_fn) error_fn(error)
-        } else {
-          console.debug(body)
-          fn(body, response)
-        }
-      })
+      url: url,
+      method: 'GET',
+      json: true
+    }, function (error, response, body) {
+      if (error) {
+        console.error({ ajaxcall: 'get' }, error)
+        if (errorFn) errorFn(error)
+      } else {
+        console.debug(body)
+        fn(body, response)
+      }
+    })
   },
-  post: function (url, data, fn, error_fn) {
+  post: function (url, data, fn, errorFn) {
     request({
-        url: url,
-        method: 'POST',
-        json: true,
-        body: data
-      }, function (error, response, body){
-        if (error) {
-          console.error({ ajaxcall: 'post' }, error)
-          if (error_fn) error_fn(error)
-        } else {
-          console.debug(body)
-          fn(body, response)
-        }
-      })
+      url: url,
+      method: 'POST',
+      json: true,
+      body: data
+    }, function (error, response, body) {
+      if (error) {
+        console.error({ ajaxcall: 'post' }, error)
+        if (errorFn) errorFn(error)
+      } else {
+        console.debug(body)
+        fn(body, response)
+      }
+    })
   }
 }
